c82cfa3cf9cb9948e771f405b2fd22a8de3616d7
Ivancarrasco <rice_nos@hotmail.com>
2018-11-28 14:50:54 -0600

    inicial

---
+++ app/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+var routes        = require( './routes' );
+
+app.use(bodyParser.json());
+
+app.get('/', routes.index);
+app.post('/',routes.store);
+app.get('/:id',routes.search);
+app.delete('/:id',routes.erase);
+app.put('/:id',routes.replace);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
